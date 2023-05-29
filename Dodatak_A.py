2ff6dc1763d8ccb665ddeb53ca869f47c0807d30
Danijel Barišić <skrillexe00@gmail.com>
2023-05-30 01:15:03 +0200

    Fixed files

--- Dodatak_A.py
+++ Dodatak_A.py
@@ -1,5 +1,7 @@
+import ast
 import os
 import getpass
+
 
 class OperationsManager():
 
@@ -9,6 +11,8 @@ class OperationsManager():
 
     def perform_division(self) -> float:
         """Divides a with b. If b is zero, returns NaN."""
+        if self.b == 0:
+            return float('nan')
         return self.a / self.b
 
 
@@ -17,17 +21,36 @@ def login_success():
     b = float(input("B = "))
     ops_manager = OperationsManager(a, b)
     print(ops_manager.perform_division())
- 
+
     expression = input('Enter a mathematical formula to calculate: ')
-    print("Result: ", eval(expression))
+    try:
+        result = ast.literal_eval(expression)
+        print("Result:", result)
+    except (ValueError, SyntaxError):
+        print("Invalid mathematical expression!")
 
 
 def main():
     user = input("Username: ")
     password = getpass.getpass("Password: ")
-    if user != "root" or password != "123":
+    if not authenticate(user, password):
         print("Wrong username or password!")
         exit(0)
-    else:
-        print("Login success!")
-        login_success()
+
+    print("Login success!")
+    login_success()
+
+
+def authenticate(user, password):
+    credentials_file = "credentials.txt"
+
+    with open(credentials_file, 'r') as file:
+        for line in file:
+            key, value = line.strip().split(':')
+            if key.strip() == 'username' and user == value.strip():
+                next_line = next(file, None)
+                if next_line:
+                    key, value = next_line.strip().split(':')
+                    if key.strip() == 'password' and password == value.strip():
+                        return True
+    return False
